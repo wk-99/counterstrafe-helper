98cae3a2aac6fc192fbd90cc434e11a3a4d9427a
William Kang <williamkang99@gmail.com>
2025-12-28 23:39:13 -0800

    MAINT: Possible fix for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/counterstrafe-helper',
+  base: '/counterstrafe-helper/',
   server: {
     port: 5173,
     watch: {
