4f07923a9e2ec8d88a78c825cfef8e09fa75fe64
William Kang <williamkang99@gmail.com>
2025-12-28 23:29:46 -0800

    MAINT: gh-pages updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/counterstrafe-helper',
   server: {
     port: 5173,
     watch: {
